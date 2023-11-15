8eb3957ba026c8ae27632392bb6fc9b6038116e6
Galih Prayoga <galihp83@gmail.com>
2023-11-15 13:01:46 +0700

    buat api CRUD products

--- day22/main.go
+++ day22/main.go
@@ -5,6 +5,7 @@ import (
 	"day-22/config"
 	"day-22/customer"
 	"day-22/model"
+	"day-22/products"
 	"day-22/users"
 	"fmt"
 
@@ -22,10 +23,12 @@ func main() {
 	db.AutoMigrate(&model.User{})
 	db.AutoMigrate(&model.Customer{})
 	db.AutoMigrate(&model.User{})
+	db.AutoMigrate(&model.Product{})
 
 	var auth = auth.AuthSystem{DB: db}
 	var cust = customer.CustomerSystem{DB: db}
 	var user = users.UserSystem{DB: db}
+	var product = products.ProductSystem{DB: db}
 
 	e := echo.New()
 
@@ -48,6 +51,13 @@ func main() {
 	e.GET("/users/:username", user.ReadUserById)
 	e.DELETE("/users/:username", user.DeleteUser)
 	e.PUT("/users/:username", user.UpdateUser)
+
+	// Product
+	e.POST("/products", product.CreateProduct)
+	e.GET("/products", product.ReadProduct)
+	e.GET("/products/:barcode", product.ReadProductById)
+	e.DELETE("/products/:barcode", product.DeleteProduct)
+	e.PUT("/products/:barcode", product.UpdateProduct)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
---
+++ day22/products/products.go
@@ -0,0 +1,157 @@
+package products
+
+import (
+	"day-22/model"
+	"errors"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type ProductSystem struct {
+	DB *gorm.DB
+}
+
+func (ps *ProductSystem) CreateProduct(c echo.Context) error {
+	var newProduct = new(model.Product)
+	if err := c.Bind(newProduct); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]any{
+			"message": err.Error(),
+			"data":    nil,
+		})
+	}
+
+	qry := ps.DB.Create(newProduct)
+	err := qry.Error
+
+	if err != nil {
+		c.Echo().Logger.Error("Database error :", err.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]any{
+			"message": "Terjadi kesalahan pada pengolahan data",
+			"data":    nil,
+		})
+	}
+
+	return c.JSON(http.StatusOK, newProduct)
+}
+
+func (ps *ProductSystem) ReadProduct(c echo.Context) error {
+	var ProductList []model.Product
+
+	qry := ps.DB.Find(&ProductList)
+	err := qry.Error
+
+	if err != nil {
+		c.Echo().Logger.Error("Database error:", err.Error())
+		return c.JSON(http.StatusNotFound, map[string]interface{}{
+			"message": "Data tidak ditemukan",
+			"data":    nil,
+		})
+	}
+
+	return c.JSON(http.StatusOK, ProductList)
+}
+
+func (ps *ProductSystem) DeleteProduct(c echo.Context) error {
+	ProductID := c.Param("barcode")
+
+	var product model.Product
+
+	qry := ps.DB.Where("barcode = ?", ProductID).First(&product)
+	if qry.Error != nil {
+		if errors.Is(qry.Error, gorm.ErrRecordNotFound) {
+			return c.JSON(http.StatusNotFound, map[string]interface{}{
+				"message": "Data tidak ditemukan",
+				"data":    nil,
+			})
+		}
+		c.Echo().Logger.Error("Database error:", qry.Error.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Terjadi kesalahan pada pengolahan data",
+			"data":    nil,
+		})
+	}
+
+	err := ps.DB.Delete(&product).Error
+	if err != nil {
+		c.Echo().Logger.Error("Database error:", err.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Terjadi kesalahan pada penghapusan data",
+			"data":    nil,
+		})
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func (ps *ProductSystem) UpdateProduct(c echo.Context) error {
+	ProductID := c.Param("barcode")
+
+	var product model.Product
+
+	qry := ps.DB.Where("barcode = ?", ProductID).First(&product)
+	if qry.Error != nil {
+		if errors.Is(qry.Error, gorm.ErrRecordNotFound) {
+			return c.JSON(http.StatusNotFound, map[string]interface{}{
+				"message": "Data tidak ditemukan",
+				"data":    nil,
+			})
+		}
+		c.Echo().Logger.Error("Database error:", qry.Error.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Terjadi kesalahan pada pengolahan data",
+			"data":    nil,
+		})
+	}
+
+	var updateProduct model.Product
+
+	if err := c.Bind(&updateProduct); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "Invalid request payload",
+			"data":    nil,
+		})
+	}
+
+	err := ps.DB.Model(&product).Updates(&updateProduct).Error
+
+	if err != nil {
+		c.Echo().Logger.Error("Database error:", err.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Internal Server Error",
+			"data":    nil,
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Customer updated successfully",
+		"data":    &product,
+	})
+}
+
+func (ps *ProductSystem) ReadProductById(c echo.Context) error {
+	ProductID := c.Param("barcode")
+
+	var product model.Product
+
+	qry := ps.DB.Where("barcode = ?", ProductID).First(&product)
+	if qry.Error != nil {
+		if errors.Is(qry.Error, gorm.ErrRecordNotFound) {
+			return c.JSON(http.StatusNotFound, map[string]interface{}{
+				"message": "Data tidak ditemukan",
+				"data":    nil,
+			})
+		}
+		c.Echo().Logger.Error("Database error:", qry.Error.Error())
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Terjadi kesalahan pada pengolahan data",
+			"data":    nil,
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Success",
+		"data":    &product,
+	})
+}
