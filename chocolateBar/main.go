93fc4d8981b2cf11e43c0863f6be3bc296e1d0b2
Galih Prayoga <galihp83@gmail.com>
2024-02-11 23:39:20 +0700

    hackerrank chocolate bar

---
+++ chocolateBar/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+func birthday(s []int32, d int32, m int32) int32 {
+	var segments int32
+	var l int32 = int32(len(s))
+
+	for i := int32(0); i < l-m+1; i++ {
+		var segmentSum int32
+		segmentSquares := make([]int32, 0)
+
+		for j := int32(0); j < m; j++ {
+			segmentSum += s[i+j]
+			segmentSquares = append(segmentSquares, s[i+j])
+		}
+
+		if int32(len(segmentSquares)) == m && segmentSum == d {
+			segments++
+		}
+	}
+
+	return segments
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	sTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var s []int32
+
+	for i := 0; i < int(n); i++ {
+		sItemTemp, err := strconv.ParseInt(sTemp[i], 10, 64)
+		checkError(err)
+		sItem := int32(sItemTemp)
+		s = append(s, sItem)
+	}
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	dTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	d := int32(dTemp)
+
+	mTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	m := int32(mTemp)
+
+	result := birthday(s, d, m)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
